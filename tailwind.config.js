734dd48fbc2aa42a3c027e2fdef389388597d1c0
Andrew Ferrier <107015+andrewferrier@users.noreply.github.com>
2026-04-22 14:38:35 +0100

    chore: Remove dead code

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/client/**/*.{js,jsx,ts,tsx}', './src/client/index.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
